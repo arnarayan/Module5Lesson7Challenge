72fa08a0c285ed8e923a9372c78157774d4a4c6b
Anand.Narayan <arnarayan@gmail.com>
2022-05-12 12:21:42 -0600

    Initial Commit

---
+++ Module5Lesson7Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Module5Lesson7Challenge
+//
+//  Created by Anand Narayan on 2022-05-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Module5Lesson7Challenge/Module5Lesson7ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module5Lesson7ChallengeApp.swift
+//  Module5Lesson7Challenge
+//
+//  Created by Anand Narayan on 2022-05-12.
+//
+
+import SwiftUI
+
+@main
+struct Module5Lesson7ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
